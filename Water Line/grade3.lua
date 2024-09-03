62fa5f22518e23d1e2d6715b0265647da3f33a00
Rex Lao <u7423294@anu.edu.au>
2024-09-03 20:57:31 +1000

    Moved recipe search outside main loop

--- Water Line/grade3.lua
+++ Water Line/grade3.lua
@@ -3,24 +3,23 @@ os = require("os")
 
 me = component.me_interface
 gt = component.gt_machine
+cpu = me.getCpus()[1]
+craftables = me.getCraftables()
+
+request_me = nil
+for _, craftable in pairs(craftables) do
+    item = craftable.getItemStack()
+    if item["label"] == "Input Polyaluminium Chloride" then
+        request_me = craftable
+        break
+    end
+end
+
+if request_me == nil then
+    error("Item not found!")
+end
 
 function addFluid()
-    cpu = me.getCpus()[1]
-    craftables = me.getCraftables()
-
-    request_me = nil
-    for _, craftable in pairs(craftables) do
-        item = craftable.getItemStack()
-        if item["label"] == "Input Polyaluminium Chloride" then
-            request_me = craftable
-            break
-        end
-    end
-
-    if request_me == nil then
-        error("Item not found!")
-    end
-
     request_me.request(1, False, cpu["name"])
     os.sleep(1)
     cpu.cpu.cancel()
@@ -28,7 +27,6 @@ end
 
 while true do
     if gt.getWorkMaxProgress() == 0 then
-        -- Machine is off, come back later
         os.sleep(10)
     else
         info = gt.getSensorInformation()
