33c1de58be7f10d0b587a056449b6a0c7defb66a
Rex Lao <u7423294@anu.edu.au>
2024-09-03 18:26:26 +1000

    Initial test for grade 3 water

---
+++ Water Line/grade3.lua
@@ -0,0 +1,28 @@
+component = require("component")
+os = require("os")
+
+me = component.me_interface
+gt = component.gt_machine
+
+while true do
+    cpu = me.getCpus()[1]
+    craftables = me.getCraftables()
+
+    request_me = nil
+    for _, craftable in pairs(craftables) do
+        item = craftable.getItemStack()
+        if item["label"] == "Input Polyaluminium Chloride" do
+            request_me = craftable
+            break
+        end
+    end
+
+    if request_me == nil do
+        error("Item not found!")
+    end
+
+    request_me.request(1, False, cpu["name"])
+    os.sleep(1)
+    cpu.cpu.cancel()
+    diff = gt.getWorkMaxProgress() - gt.getWorkProgress()
+    os.sleep(diff + 3)
