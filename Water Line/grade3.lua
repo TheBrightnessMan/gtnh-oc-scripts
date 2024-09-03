1d012f18b6fcd4bae155d066ae1ed8550400bed2
Rex Lao <u7423294@anu.edu.au>
2024-09-03 19:12:32 +1000

    Added checks for machine off

--- Water Line/grade3.lua
+++ Water Line/grade3.lua
@@ -4,7 +4,7 @@ os = require("os")
 me = component.me_interface
 gt = component.gt_machine
 
-while true do
+function addFluid()
     cpu = me.getCpus()[1]
     craftables = me.getCraftables()
 
@@ -24,6 +24,23 @@ while true do
     request_me.request(1, False, cpu["name"])
     os.sleep(1)
     cpu.cpu.cancel()
-    diff = (gt.getWorkMaxProgress() - gt.getWorkProgress()) // 20
-    os.sleep(diff + 3)
+end
+
+while true do
+    if gt.getWorkMaxProgress() == 0 then
+        -- Machine is off, come back later
+        os.sleep(5)
+    else
+        info = gt.getSensorInformation()
+        amount_str = info[3]
+        length = string.len(amount_str)
+        amount = tonumber(string.sub(amount_str, 48, length - 1))
+
+        if amount == 0 then
+            addFluid()
+        end
+        
+        diff = (2400 - gt.getWorkProgress()) // 20
+        os.sleep(diff + 3)
+    end
 end
