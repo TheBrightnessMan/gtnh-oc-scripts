1c695cbf5b7122b352c08f77a76cea44ee3193de
Rex Lao <u7423294@anu.edu.au>
2024-09-09 14:09:49 +1000

    Added check for machine state

--- Water Line/grade5.lua
+++ Water Line/grade5.lua
@@ -10,20 +10,26 @@ if he_plasma == nil then error("He Plasma recipe not found!") end
 if coolant == nil then error("Coolant recipe not found!") end
 
 while True do
-    for i = 1, 3, 1 do
-        print("Loop " .. tostring(i))
-        print("Requesting 10 He Plasma recipes...")
-        ae2_lib.requestRecipeCancel(he_plasma, 10, 3, 2)
-        print("Waiting 11 seconds for plasma to be consumed...")
-        os.sleep(11)
+    print("Starting cycle...")
+    if gt.getWorkMaxProgress() == 0 then
+        print("Machine disabled, sleep 10...")
+        os.sleep(10)
+    else
+        for i = 1, 3, 1 do
+            print("Loop " .. tostring(i))
+            print("Requesting 10 He Plasma recipes...")
+            ae2_lib.requestRecipeCancel(he_plasma, 10, 3, 2)
+            print("Waiting 11 seconds for plasma to be consumed...")
+            os.sleep(11)
 
-        print("Requesting 2000 Coolant recipes...")
-        ae2_lib.requestRecipeCancel(coolant, 2000, 3, 2)
-        print("Waiting 21 seconds for plasma to be consumed...")
-        os.sleep(21)
+            print("Requesting 2000 Coolant recipes...")
+            ae2_lib.requestRecipeCancel(coolant, 2000, 3, 2)
+            print("Waiting 21 seconds for plasma to be consumed...")
+            os.sleep(21)
+        end
+        print("Completed 3 loops.")
+        sleep = ((2400 - gt.getWorkProgress()) // 20) + 3
+        print("Sleeping for " .. tonumber(sleep) .. " seconds...")
+        os.sleep(sleep)
     end
-    print("Completed 3 loops.")
-    sleep = ((2400 - gt.getWorkProgress()) // 20) + 3
-    print("Sleeping for " .. tonumber(sleep) .. " seconds...")
-    os.sleep(sleep)
 end
