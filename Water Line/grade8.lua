08d2c62d6edade58a9b85a2db62106c2005a10f5
Rex Lao <u7423294@anu.edu.au>
2024-09-10 20:28:32 +1000

    Update

--- Water Line/grade8.lua
+++ Water Line/grade8.lua
@@ -3,16 +3,26 @@ os = require("os")
 ae2_lib = require("ae2_lib")
 gt = component.gt_machine
 
+recipes = {}
+recipes[0] = ae2_lib.getRecipe("Up")
+recipes[1] = ae2_lib.getRecipe("Down")
+recipes[2] = ae2_lib.getRecipe("Top")
+recipes[3] = ae2_lib.getRecipe("Bottom")
+recipes[4] = ae2_lib.getRecipe("Strange")
+recipes[5] = ae2_lib.getRecipe("Charm")
+
+recipeSuffix = "-Quark Releasing Catalyst"
+
 quarks = {}
-quarks[0] = ae2_lib.getRecipe("Up")
-quarks[1] = ae2_lib.getRecipe("Down")
-quarks[2] = ae2_lib.getRecipe("Top")
-quarks[3] = ae2_lib.getRecipe("Bottom")
-quarks[4] = ae2_lib.getRecipe("Strange")
-quarks[5] = ae2_lib.getRecipe("Charm")
+quarks[0] = ae2_lib.getRecipe("Up" .. recipeSuffix)
+quarks[1] = ae2_lib.getRecipe("Down" .. recipeSuffix)
+quarks[2] = ae2_lib.getRecipe("Top" .. recipeSuffix)
+quarks[3] = ae2_lib.getRecipe("Bottom" .. recipeSuffix)
+quarks[4] = ae2_lib.getRecipe("Strange" .. recipeSuffix)
+quarks[5] = ae2_lib.getRecipe("Charm" .. recipeSuffix)
 
 for i = 0, 5 do
-    if quarks[i] == nil then error(tostring(i) .. " recipe not found!") end
+    if recipes[i] == nil then error(tostring(i) .. " recipe not found!") end
 end
 
 while true do
@@ -21,21 +31,33 @@ while true do
         print("Machine disabled, sleep 10...")
         os.sleep(10)
     else
-        info = gt.getSensorInformation()
-        success_String = info[#info]
-        success = string.sub(success_String, 44)
-        if success == "No" then
-            for i = 0, 4 do
-                for j = (i+1), 5 do
-                    print("Inserting " .. tostring(i) .. ", " .. tostring(j))
-                    ae2_lib.requestRecipeCancel(quarks[i], 1, 5, 1)
-                    ae2_lib.requestRecipeCancel(quarks[j], 1, 5, 1)
+        quark1 = -1
+        quark2 = -1
+        for i = 0, 4 do
+            for j = (i+1), 5 do
+                print("Inserting " .. tostring(i) .. ", " .. tostring(j))
+                ae2_lib.requestRecipeCancel(recipes[i], 1, 5, 1)
+                ae2_lib.requestRecipeCancel(recipes[j], 1, 5, 1)
+
+                info = gt.getSensorInformation()
+                success_String = info[#info]
+                success = string.sub(success_String, 44)
+                if success == "Yes" then
+                    print("Combo found! (" .. tonumber(i) .. ", " .. tonumber(j) .. ")")
+                    quark1 = i
+                    quark2 = j
+                    goto end
                 end
             end
         end
-        print("Checked all combos OR Pair found.")
+        ::end::
         sleep = ((2400 - gt.getWorkProgress()) // 20) + 3
         print("Sleeping for " .. tonumber(sleep) .. " seconds...")
         os.sleep(sleep)
+        if quark1 == -1 then error("Wait what? Quark 1 not found!") end
+        if quark2 == -1 then error("Wait what? Quark 2 not found!") end
+        print("Requesting replacements...")
+        quarks[quark1].request(1)
+        quarks[quark2].request(1)
     end
 end
