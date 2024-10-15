94531e8d9039d9136146df185d5da228bea0c6a2
Bright <60181861+TheBrightnessMan@users.noreply.github.com>
2024-10-16 10:14:38 +1100

    Update grade7.lua

--- Water Line/grade7.lua
+++ Water Line/grade7.lua
@@ -5,14 +5,14 @@ gt = component.gt_machine
 redstone = component.redstone
 
 bit1_input = {}
-bit1_input[0] = ae2_lib.getRecipe("He")
-bit1_input[1] = ae2_lib.getRecipe("Ne")
-bit1_input[2] = ae2_lib.getRecipe("Kr")
-bit1_input[3] = ae2_lib.getRecipe("Xe")
+bit1_input[0] = ae2_lib.getRecipe("Grade 7 He")
+bit1_input[1] = ae2_lib.getRecipe("Grade 7 Ne")
+bit1_input[2] = ae2_lib.getRecipe("Grade 7 Kr")
+bit1_input[3] = ae2_lib.getRecipe("Grade 7 Xe")
 
-superconductor = ae2_lib.getRecipe("Superconductor")
-neutronium = ae2_lib.getRecipe("Nt")
-coolant = ae2_lib.getRecipe("grade7 Coolant")
+superconductor = ae2_lib.getRecipe("Grade 7 Superconductor")
+neutronium = ae2_lib.getRecipe("Grade 7 Nt")
+coolant = ae2_lib.getRecipe("Grade 7 Coolant")
 
 if bit1_input[0] == nil then error("He recipe not found!") end
 if bit1_input[1] == nil then error("Ne recipe not found!") end
@@ -20,7 +20,7 @@ if bit1_input[2] == nil then error("Kr recipe not found!") end
 if bit1_input[3] == nil then error("Xe recipe not found!") end
 if superconductor == nil then error("Superconductor recipe not found!") end
 if neutronium == nil then error("Nt recipe not found!") end
-if coolant == nil then error("grade7 Coolant recipe not found!") end
+if coolant == nil then error("Coolant recipe not found!") end
 
 while true do
     print("Starting cycle...")
@@ -33,7 +33,7 @@ while true do
 
         if signal == 0 then
             print("Overloaded! Inserting Super Coolant...")
-            ae2_lib.requestRecipeCancel(coolant, 1, 4, 2)
+            ae2_lib.requestRecipeCancel(coolant, 1, "7", 2)
         else
             bit1 = signal & 1
             bit2 = (signal >> 1) & 1
@@ -46,19 +46,19 @@ while true do
                     print("Bit 1 set, inserting gas...")
                     gasId = (signal >> 1) & 3
                     gas = bit1_input[gasId]
-                    ae2_lib.requestRecipeCancel(gas, 1, 4, 2)
+                    ae2_lib.requestRecipeCancel(gas, 1, "7", 2)
                     print("Bit 1 decode successful!")
                 end
 
                 if bit2 == 1 then
                     print("Bit 2 set, inserting superconductor...")
-                    ae2_lib.requestRecipeCancel(superconductor, 1, 4, 2)
+                    ae2_lib.requestRecipeCancel(superconductor, 1, "7", 2)
                     print("Bit 2 decode successful!")
                 end
 
                 if bit3 == 1 then
                     print("Bit 3 set, inserting neutronium...")
-                    ae2_lib.requestRecipeCancel(neutronium, 1, 4, 2)
+                    ae2_lib.requestRecipeCancel(neutronium, 1, "7", 2)
                     print("Bit 3 decode successful!")
                 end
             end
